69998180b1ec86a2f33f98952cb15f34c3f7f187
Dmytro Hryshchenko <puzz3d@gmail.com>
2022-09-12 23:56:22 +0300

    testParallel method added.

--- src/test/java/bbc/BBCTest.java
+++ src/test/java/bbc/BBCTest.java
@@ -1,4 +1,17 @@
 package bbc;
 
-public class BBCTest {
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BBCTest {
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:bbc")
+                //.outputCucumberJson(true)
+                .parallel(2);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
 }
